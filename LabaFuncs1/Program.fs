347083e6604fecfef78e434837ad4f6086c3392a
Alex <sasha_novosel@mail.ru>
2024-10-30 23:48:08 +0300

    Добавьте файлы проекта.

---
+++ LabaFuncs1/Program.fs
@@ -0,0 +1,9 @@
+﻿
+let rec sumNumbers a cN n =
+    if (cN - n) = 0 then a
+    else
+        printf "(%d - %d * %d) = %d \n" a (cN - n) cN (int64(a) - int64(cN - n) * int64(cN))
+        (int64(a) - int64(cN - n) * int64(cN)) * (sumNumbers a cN (n + 1))
+
+
+printfn "Результат: %d" (sumNumbers (int(System.Console.ReadLine())) (int(System.Console.ReadLine())) 0)
